b66af268d2cb034886facea6148f128b63f05861
Hanson Wang <hansonw@users.noreply.github.com>
2025-05-23 20:39:23 -0700

    feat: add branch listing command

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -11,6 +11,7 @@ import (
 	"github.com/mroth/scmpuff/internal/cmd/inits"
 	"github.com/mroth/scmpuff/internal/cmd/intro"
 	"github.com/mroth/scmpuff/internal/cmd/status"
+	"github.com/mroth/scmpuff/internal/commands/branch"
 	"github.com/spf13/cobra"
 )
 
@@ -49,6 +50,7 @@ If you are just getting started, try the intro!`,
 	rootCmd.AddCommand(expand.NewExpandCmd())
 	rootCmd.AddCommand(inits.NewInitCmd())
 	rootCmd.AddCommand(status.NewStatusCmd())
+	rootCmd.AddCommand(branch.CommandBranch())
 
 	return rootCmd
 }
